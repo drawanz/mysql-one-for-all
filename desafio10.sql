43c2da324bed5a5db331e37cc9d77fff95dec923
Leonidas <leonidas.drawanz@gmail.com>
2022-06-04 16:40:51 -0300

    req 11

--- desafio10.sql
+++ desafio10.sql
@@ -1,4 +1,13 @@
-SELECT c.cancao AS nome, COUNT(h.cancao_id) AS reproducoes FROM SpotifyClone.historico AS h
-INNER JOIN SpotifyClone.cancao AS c ON c.cancao_id = h.cancao_id
-WHERE usuario_id = 1 OR usuario_id = 4 OR usuario_id = 5
+SELECT 
+    c.cancao AS nome, COUNT(h.cancao_id) AS reproducoes
+FROM
+    SpotifyClone.plano AS p
+        INNER JOIN
+    SpotifyClone.usuario AS u ON u.plano_id = p.plano_id
+        INNER JOIN
+    SpotifyClone.historico AS h ON h.usuario_id = u.usuario_id
+        INNER JOIN
+    SpotifyClone.cancao AS c ON c.cancao_id = h.cancao_id
+WHERE
+    plano = 'gratuito' OR plano = 'pessoal'
 GROUP BY c.cancao;
