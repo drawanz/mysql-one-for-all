8949c706ba745411372209a1754a1a4b96d22767
Leonidas <leonidas.drawanz@gmail.com>
2022-06-04 10:03:49 -0300

    req 4

---
+++ desafio4.sql
@@ -0,0 +1,4 @@
+SELECT (u.usuario) AS usuario, IF (YEAR(MAX(h.data_reproducao)) = 2021, 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+FROM SpotifyClone.usuario AS u
+INNER JOIN SpotifyClone.historico as h ON h.usuario_id = u.usuario_id
+GROUP BY u.usuario;
